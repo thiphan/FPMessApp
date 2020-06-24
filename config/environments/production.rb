a7bf8894f7bd7f850f8fbae5683ebd3dc5267e59
Thi Phan <thoaptk07@gmail.com>
2020-06-24 16:11:38 -0500

    update rails version

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,7 +52,7 @@ Rails.application.configure do
 
   # Use a real queuing backend for Active Job (and separate queues per environment).
   # config.active_job.queue_adapter     = :resque
-  # config.active_job.queue_name_prefix = "FPMessApp_production"
+  # config.active_job.queue_name_prefix = "fp_mess_app_production"
 
   config.action_mailer.perform_caching = false
 
